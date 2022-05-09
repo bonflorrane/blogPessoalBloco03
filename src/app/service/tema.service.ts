48fcff28027906291a83e56891f17c497c4b6427
bonflorrane <bonfim_lorrane@hotmail.com>
2022-05-09 14:04:28 -0300

    Implementação put-delete de Tema

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -24,4 +24,16 @@ export class TemaService { // será add os métodos de consumo da API da model T
     return this.http.post<Tema>('http://localhost:8080/tema', tema, this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`http://localhost:8080/tema/${id}`, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('http://localhost:8080/tema', tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)
+  }
+
 }
